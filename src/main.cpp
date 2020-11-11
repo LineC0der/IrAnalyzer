164e645d784e157dca1df20488a8b3167ea0e82a
LineC0der <jonas.volk@gmx.de>
2020-11-11 15:03:16 +0100

    Added Protocol recognition

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,28 @@ void setup() {
 
 void loop() {
   if (irrecv.decode(&results)) {
-    // print() & println() can't handle printing long longs. (uint64_t)
+    Serial.print("Recieved: ");
     serialPrintUint64(results.value, HEX);
-    Serial.println("");
+    Serial.print(" - with Protocol: ");
+    switch (results.decode_type){
+      case NEC: Serial.println("NEC"); break;
+      case SONY: Serial.println("SONY"); break;
+      case RC5: Serial.println("RC5"); break;
+      case RC6: Serial.println("RC6"); break;
+      case DISH: Serial.println("DISH"); break;
+      case SHARP: Serial.println("SHARP"); break;
+      case JVC: Serial.println("JVC"); break;
+      case SANYO: Serial.println("SANYO"); break;
+      case MITSUBISHI: Serial.println("MITSUBISHI"); break;
+      case SAMSUNG: Serial.println("SAMSUNG"); break;
+      case LG: Serial.println("LG"); break;
+      case WHYNTER: Serial.println("WHYNTER"); break;
+      case AIWA_RC_T501: Serial.println("AIWA_RC_T501"); break;
+      case PANASONIC: Serial.println("PANASONIC"); break;
+      case DENON: Serial.println("DENON"); break;
+    default:
+      case UNKNOWN: Serial.println("UNKNOWN"); break;
+    }
     irrecv.resume();  // Receive the next value
   }
   delay(100);
